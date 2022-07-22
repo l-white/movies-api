851d179463b1a7dfab8db8c1125199de5a13cf57
Laura <law7676@outlook.com>
2022-07-22 11:16:37 -0500

    added autoIncrementID

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace movies_api.Models {
   public class Movie {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int id { get; set; }
     public string? title { get; set; }
     public string? poster { get; set; }
